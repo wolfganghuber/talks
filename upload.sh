649efa4c68e5f435ed9fff1adb2f7ef1d881c9fe
Wolfgang Huber <wolfgang.huber@embl.org>
2025-07-20 15:36:16 +0300

    Note on upload to S3 bucket

---
+++ upload.sh
@@ -0,0 +1,13 @@
+# Regression+testing/
+cd clones/talks/regression+testing/
+mkdir regression+testing
+mv wh.scss index.html index_files regression+testing
+mc mirror --overwrite regression+testing wwwhuber/www-huber/users/whuber/talks/regression+testing
+
+# Multivariate/PCA
+cd ownCloud/talks
+mc cp 2025-UBDS3-MultivariateAnalysis.pdf wwwhuber/www-huber/users/whuber/talks/
+
+# ML
+cd ownCloud/talks/2102-MoBi/
+mc cp Lec-ML.pdf wwwhuber/www-huber/users/whuber/talks/
